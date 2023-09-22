b2b7798ebbb839e988887c5fa4debeca4edcd390
kusumanc <145649007+kusumanc@users.noreply.github.com>
2023-09-22 20:52:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import snowflake.connector
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header(' Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
